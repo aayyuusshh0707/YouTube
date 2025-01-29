1182502620049398d4c3e1c26974071fa5c3041b
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-01-29 15:17:58 +0530

    Add initial backend setup with Prettier configuration and dotenv support

---
+++ Backend/src/index.js
@@ -0,0 +1,14 @@
+
+//if-e function
+
+// (async()=>{
+//    try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     console.log(`Connected to MongoDB ${DB_NAME}`)
+//    } catch (error) {
+//     console.error('Error connecting to MongoDB', error);
+//     throw error;
+//    }
+
+
+// })()
