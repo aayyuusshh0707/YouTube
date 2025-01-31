3585c4aeb6d35093896e7300aeb49c96fdc588ba
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-01-31 17:18:48 +0530

    Implement user registration route and async error handling; refactor app.js for route separation and improve server startup logging

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,42 +4,28 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+//middlewares
 app.use(
-
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // use for url encoded data
-//  object in object extended: true
+app.use(express.urlencoded({ extended: true })); // use for url encoded data ,  object in object extended: true
 app.use(express.static("public")); // use for static files like images, css, js
-
 app.use(cookieParser()); // use for parsing cookies
-export default app;
 
+//routes
+import router from "./routes/user.routes.js";
 
+//routes declaration
+app.use("/api/users", router);   // we seprate the routes so , we use middleware to use the routes
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default  app;
 
 //app.js operations are:
 //1. call db function
 //2. call dotenv.config()
 //3. use cors, json, urlencoded, static, cookieParser (middleware)
 //4. call app.listen() function
-
-
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,19 +2,16 @@ import connectDB from "./db/db.js";
 import dotenv from "dotenv";
 import app from "./app.js";
 
-const port = process.env.PORT || 5000;
-
 dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
-    app.listen(port , () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB !!", error);
   });
-   
-
-
