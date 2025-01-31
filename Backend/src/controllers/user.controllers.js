2938120ff7dcbd5ea06d8219ee5d2c24da0a819b
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-01-31 17:58:27 +0530

    Enhance user registration process with data validation and file upload support; refactor error handling and middleware integration

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -1,9 +1,27 @@
-import  asyncHandler from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
-});
+  //things to do here   -->>
+  //get data from the frontend
+  //validate data - not empty{ email, password, confirm password}
+  //check if user already exists {check by email, username}
+  //check img and avatar {if not present set default img}
+  //uplode them to cloudinary
+  //create user object - create entry in DB
+  //remove password ,refresh token from the response
+  //check for user creation
+  //send response
 
+  //get data from the frontend
+
+  const { fullname, username, email, password } = req.body;
+  console.log(email);
+
+  if(fullname=="" || username=="" || email=="" || password==""){
+    throw new ApiError(400, "All fields are required");
+  }
+});
+ 
 export default registerUser;
